27f9949866c7ffcac59397bc3f17f3d27bc62202
sebseth21a <xonnekpaucarsilva2@gmail.com>
2026-06-25 03:03:52 -0500

    Add files via upload

---
+++ SistemaHistoriasClinicas01/src/principal/Principal.java
@@ -0,0 +1,14 @@
+package principal;
+
+import vista.FrmLogin;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        FrmLogin login = new FrmLogin();
+        login.setVisible(true);
+
+    }
+
+}
